258314f5035b57c6a53df8c6d5995e88b172a82b
INYONG KIM <ohnyong87@gmail.com>
2024-03-25 14:11:52 +0900

    feat : Header, Footer Layout 설정 및 템플릿 참조

--- src/main/java/com/example/demoapp/controller/FirstController.java
+++ src/main/java/com/example/demoapp/controller/FirstController.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
-    @GetMapping("/")
+    @GetMapping("/hi")
     public String hi(Model model){
         model.addAttribute("username", "Hello World!");
         return "hi";
     }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("username", "Hello World!");
+        return "index";
+    }
+
 
 }
