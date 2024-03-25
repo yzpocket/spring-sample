6c2a505d599b5dc972508d2275ac0357c14b2dc3
INYONG KIM <ohnyong87@gmail.com>
2024-03-25 16:29:53 +0900

    feat : ResponseBody로 데이터 자체 반환, JSON 데이터 반환

--- src/main/java/com/example/demoapp/controller/HelloController.java
+++ src/main/java/com/example/demoapp/controller/HelloController.java
@@ -4,22 +4,48 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String Hello(Model model){
         model.addAttribute("data", "user1");
         return "hello";
     }
 
-    @GetMapping("/hello-mvc") // Http 요청시 ?name={value} 쿼리스트링필요
+    @GetMapping("hello-mvc") // Http 요청시 ?name={value} 쿼리스트링필요
     public String helloMvc(@RequestParam(name = "name") String name, Model model){
         //쿼리스트링으로 담겨온 데이터를 name변수에 담기고
         //Model의 name이란 key값에 value에 위 값이 할당되어 뷰엔진으로 전달.
         model.addAttribute("name", name);
         //뷰엔진이 위 상태를 담아서 hello-template.html에 데이터를 담아서 반환
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
