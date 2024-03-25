9205d97683fa579ae5ad61f73ee4a5b6561b1481
INYONG KIM <ohnyong87@gmail.com>
2024-03-25 13:45:35 +0900

    feat : Model의 Add Attribute를 통한 뷰템플릿에 변수 넣기

--- src/main/java/com/example/demoapp/controller/FirstController.java
+++ src/main/java/com/example/demoapp/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.demoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/")
-    public String hi(){
+    public String hi(Model model){
+        model.addAttribute("username", "Hello World!");
         return "hi";
     }
 
