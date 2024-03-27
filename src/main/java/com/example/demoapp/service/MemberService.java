3cbbc1ab4e0fd9623f6e4e70d960f67e2f999859
INYONG KIM <ohnyong87@gmail.com>
2024-03-27 13:45:16 +0900

    feat : Member 서비스 계층 기본 C,R 비지니스로직 작성

---
+++ src/main/java/com/example/demoapp/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.example.demoapp.service;
+
+import com.example.demoapp.domain.Member;
+import com.example.demoapp.repository.MemberRepository;
+import com.example.demoapp.repository.MemoryMemberRepository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        //같은 이름 제외하는 경우
+        validateDuplicateMemberName(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMemberName(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
