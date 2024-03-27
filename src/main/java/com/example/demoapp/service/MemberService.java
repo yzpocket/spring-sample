186a0977d253d1ae5cb1d59c92e9baa0e3c735b0
INYONG KIM <ohnyong87@gmail.com>
2024-03-27 14:37:51 +0900

    feat : 어노테이션을 통한 3 Layer 계층의 컴포넌트 설정(스프링 빈 등록) 및 계층간 의존성주입(DI)

--- src/main/java/com/example/demoapp/service/MemberService.java
+++ src/main/java/com/example/demoapp/service/MemberService.java
@@ -2,16 +2,18 @@ package com.example.demoapp.service;
 
 import com.example.demoapp.domain.Member;
 import com.example.demoapp.repository.MemberRepository;
-import com.example.demoapp.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository)  {
         this.memberRepository = memberRepository;
     }
