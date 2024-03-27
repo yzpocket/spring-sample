900b79b12c1d9718045adca2c8cace0fc38de442
INYONG KIM <ohnyong87@gmail.com>
2024-03-27 15:15:15 +0900

    feat : 회원가입 HTML form태그를 통한 post 요청 확인

---
+++ src/main/java/com/example/demoapp/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.demoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
