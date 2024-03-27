958c30eb88df58bc403b2ec067e6264184c3fee9
INYONG KIM <ohnyong87@gmail.com>
2024-03-27 14:45:09 +0900

    test : 수동 빈 등록으로 변경확인

---
+++ src/main/java/com/example/demoapp/config/SpringConfig.java
@@ -0,0 +1,21 @@
+package com.example.demoapp.config;
+
+import com.example.demoapp.repository.MemberRepository;
+import com.example.demoapp.repository.MemoryMemberRepository;
+import com.example.demoapp.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
