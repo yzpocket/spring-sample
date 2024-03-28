b61ff8600400d824010e40c37d2287d51e6efbe4
INYONG KIM <ohnyong87@gmail.com>
2024-03-28 14:52:37 +0900

    feat : H2 DB로 교체한 JDBC 코드

--- src/main/java/com/example/demoapp/config/SpringConfig.java
+++ src/main/java/com/example/demoapp/config/SpringConfig.java
@@ -1,21 +1,32 @@
-//package com.example.demoapp.config;
-//
-//import com.example.demoapp.repository.MemberRepository;
-//import com.example.demoapp.repository.MemoryMemberRepository;
-//import com.example.demoapp.service.MemberService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SpringConfig {
-//
-//    @Bean
-//    public MemberService memberService(){
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//    }
-//}
+package com.example.demoapp.config;
+
+import com.example.demoapp.repository.JdbcMemberRepository;
+import com.example.demoapp.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    //@Bean
+    //public MemberService memberService(){
+    //    return new MemberService(memberRepository());
+    //}
+    //
+
+    @Bean
+    public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
